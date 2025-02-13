79bcde4c8ff09771656d8211ce657fd22fd8e52d
James Kim <kkgg3781@gmail.com>
2025-02-12 19:42:39 -0500

    got rid of alert

--- docs/script/GIMPhex2dec.js
+++ docs/script/GIMPhex2dec.js
@@ -31,8 +31,5 @@ $(document).ready(function(){
 
         // Copy the text inside the text field
         navigator.clipboard.writeText(copyText.value);
-
-        // Alert the copied text
-        alert("Copied the text: " + copyText.value)
     });
 });
